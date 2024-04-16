ef4b1eaa27c48eb58931bd12ec0c2115adc92528
Daniel Ochoa <danochoa@publicisgroupe.net>
2024-04-16 11:40:34 -0500

    testing some changes

--- src/main/java/com/ps/demoblaze/pages/NavBar.java
+++ src/main/java/com/ps/demoblaze/pages/NavBar.java
@@ -1,4 +1,30 @@
 package com.ps.demoblaze.pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class NavBar {
+    private final WebDriver driver;
+
+    private final WebDriverWait wait;
+
+    @FindBy(id = "cartur")
+    private WebElement cartLink;
+
+    public NavBar(WebDriver driver) {
+        this.driver = driver;
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        PageFactory.initElements(driver, this);
+    }
+
+    public WebElement getCartLink() {
+        wait.until(ExpectedConditions.visibilityOf(cartLink));
+        return cartLink;
+    }
 }
--- src/main/java/com/ps/demoblaze/pages/ProductDetailsPage.java
+++ src/main/java/com/ps/demoblaze/pages/ProductDetailsPage.java
@@ -1,4 +1,56 @@
 package com.ps.demoblaze.pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class ProductDetailsPage {
+    private final WebDriver driver;
+
+    @FindBy(id = "more-information")
+    private WebElement detailsInfo;
+
+    @FindBy(css = ".price-container")
+    private WebElement price;
+
+    @FindBy(css = "h2.name")
+    private WebElement productName;
+
+    @FindBy(css = ".btn.btn-success")
+    private WebElement addToCartBtn;
+
+    public ProductDetailsPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public WebElement getDetailsInfo() {
+        return detailsInfo;
+    }
+
+    public WebElement getPrice() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOf(price));
+
+        return price;
+    }
+
+    public WebElement getAddToCartBtn() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOf(addToCartBtn));
+
+        return addToCartBtn;
+    }
+
+    public WebElement getProductName() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOf(productName));
+
+        return productName;
+    }
 }
